481395ca139d7f74aaaeaff182dab59a711abf1a
nicolurquieta <nicolurquieta4@gmail.com>
2026-08-27 17:43:22 -0400

    12354

---
+++ 6.clase18082026/js/miscript.js
@@ -0,0 +1,27 @@
+alert("si fuciona")
+fuction multiplicacion(a ,b){
+    let res=0;
+    for (let i=1;i<6;i++){
+        res=res+a;
+    }
+    return (res);
+}
+fuction divicion(a,b){
+    let r=0
+    while(a,0){
+        a=a-b;
+        r=r+1;
+    }
+    return 0;
+}
+{
+cout<<"la multiplicacion es."
+cout<<multiplicar(a,b<<endl);
+cout<<"la divicion es :";
+cout<<divida(a,b)<<endl;
+return 0;
+}
+
+
+
+
